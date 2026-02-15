2611b6d62f44881c138c4e47c1bb6b113bf3205a
kranthi-kumar-2004 <159746543+kranthi-kumar-2004@users.noreply.github.com>
2026-02-15 14:48:57 +0530

    Update CrossOrigin annotation to allow all origins

--- e-com-backend/src/main/java/com/kranthi/loginpage/controller/ProductController.java
+++ e-com-backend/src/main/java/com/kranthi/loginpage/controller/ProductController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins="http://localhost:5173")
+@CrossOrigin(origins="*")
 public class ProductController {
     @Autowired
     private ProductRepository repo;
